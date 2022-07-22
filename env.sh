e8a15bb2190e7ed41ffb5981695c8df250420d6e
Joyce Babu <joyce@ennexa.com>
2022-07-22 13:59:53 +0530

    feat: detect tmux session and source configuration
    instead of opening a new tmux session

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TMP_DIR=`mktemp -u 2>/dev/null || echo "/tmp/tmp"`
-ENV_DIR="$TMP_DIR/jb-$USER-tmux"
+ENV_DIR="`dirname $TMP_DIR`/jb-$USER-tmux"
 ENV_UID=`id -u`
 
 if [ -e "$ENV_DIR" ] && [ -z "`find "$ENV_DIR" -user "$ENV_UID" -print -prune -o -prune`" ]; then
@@ -10,19 +10,41 @@ if [ -e "$ENV_DIR" ] && [ -z "`find "$ENV_DIR" -user "$ENV_UID" -print -prune -o
 fi
 
 mkdir -p "$ENV_DIR"
-
 chmod 0700 "$ENV_DIR"
 
-curl -s https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/tmux/.tmux.conf > "$ENV_DIR/.tmux.conf"
-curl -s https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/vim/.config/nvim/basic.vim > "$ENV_DIR/.vimrc"
+dl_config_file() {
+  curl -s https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/$1 > "$ENV_DIR/$2"
+  chmod 0600 "$ENV_DIR/$2"
+}
 
-find "$ENV_DIR" -type f -exec chmod 0600 {} +
-
-chmod -R 0600 "$ENV_DIR"
-chmod 0700 "$ENV_DIR"
+check_for_executable() {
+  echo "checking for $1"
+  type $1 >/dev/null 2>/dev/null
+}
 
 export MYVIMRC=$ENV_DIR/.vimrc
-
 export VIMINIT=":set runtimepath^=/tmp/jb-vim/.vim|:source $MYVIMRC"
 
-tmux -f"$ENV_DIR/.tmux.conf" new
+if check_for_executable nvim; then
+  alias vim='abc'
+elif ! check_for_executable vim; then
+  alias vim='vi'
+fi
+
+dl_config_file vim/.config/nvim/basic.vim .vimrc
+
+if check_for_executable tmux; then
+  echo "tmux found"
+  # tmux installation detected
+  if [ -n "$TMUX" ]; then
+    # Reload configuration if we are under a tmux session
+    dl_config_file tmux/.tmux.conf .tmux.conf
+    tmux source "$ENV_DIR/.tmux.conf"
+  elif [ "`echo $TERM | cut -d- -f1`" != "screen" ]; then
+    # Create a new tmux session, unless we are in a tmux session owned by
+    # another user (after sudo su - user)
+    dl_config_file tmux/.tmux.conf .tmux.conf
+    tmux -f"$ENV_DIR/.tmux.conf" new
+  fi
+fi
+
