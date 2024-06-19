9be03ad6241e4679f37453ec6f2a3072c39be915
Joyce Babu <joyce@ennexa.com>
2024-06-19 19:51:28 +0530

    fix: [env] sudo su not working correctly

--- env.sh
+++ env.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
 
-JB_TMP_DIR=`mktemp -u 2>/dev/null || echo "/tmp/tmp"`
-JB_ENV_DIR="`dirname $JB_TMP_DIR`/jb-$USER-tmux"
-ENV_UID=`id -u`
+JB_USER=$(id -un)
+JB_TMP_DIR=$(mktemp -u 2>/dev/null || echo "/tmp/tmp")
+JB_ENV_DIR="$(dirname $JB_TMP_DIR)/jb-$JB_USER-tmux"
+ENV_UID=$(id -u)
 
-if [ -e "$JB_ENV_DIR" ] && [ -z "`find "$JB_ENV_DIR" -user "$ENV_UID" -print -prune -o -prune`" ]; then
-  echo "The config directory '$JB_ENV_DIR' is not owned by $USER."
+if [ -e "$JB_ENV_DIR" ] && [ -z "$(find "$JB_ENV_DIR" -user "$ENV_UID" -print -prune -o -prune)" ]; then
+  echo "The config directory '$JB_ENV_DIR' is not owned by $JB_USER."
   exit 1
+fi
+
+if [ -z "$SHELL" ]; then
+  SHELL=$(ps | grep "^\s*$$\s" | sed -e "s/^ *$$ .* //" -e 's/^-//')
+fi
+
+jb_check_for_executable() {
+  type "$1" >/dev/null 2>/dev/null
+}
+
+jb_dl_config_file() {
+  $JB_FETCH_EXE $JB_FETCH_FLAGS https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/$1 > "$JB_ENV_DIR/$2"
+  chmod 0644 "$JB_ENV_DIR/$2"
+}
+
+JB_FETCH_EXE='wget'
+JB_FETCH_FLAGS='-q -O-'
+
+if jb_check_for_executable curl; then
+  JB_FETCH_EXE='curl'
+  JB_FETCH_FLAGS='-s'
+fi
+
+if [ -z "$JB_SKIP_TMUX_UPDATE" ] && ! jb_check_for_executable tmux; then
+  JB_SKIP_TMUX_UPDATE=1
 fi
 
 mkdir -p "$JB_ENV_DIR"
 chmod 0755 "$JB_ENV_DIR"
 
-jb_dl_config_file() {
-  if jb_check_for_executable curl; then
-    curl -s https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/$1 > "$JB_ENV_DIR/$2"
-  else
-    wget -q -O- https://raw.githubusercontent.com/JoyceBabu/dotfiles/master/$1 > "$JB_ENV_DIR/$2"
-  fi
-  chmod 0644 "$JB_ENV_DIR/$2"
-}
-
-jb_check_for_executable() {
-  type $1 >/dev/null 2>/dev/null
-}
-
 export MYVIMRC=$JB_ENV_DIR/.vimrc
-export VIMINIT=":set runtimepath^=/tmp/jb-vim/.vim|:source $MYVIMRC"
+export VIMINIT=":set runtimepath^=$JB_ENV_DIR/.vim|:source $MYVIMRC"
 
 cat <<EOF > $JB_ENV_DIR/.gitconfig
 [user]
@@ -46,7 +59,7 @@ cat <<EOF > $JB_ENV_DIR/.gitconfig
 [mergetool "nvim"]
   keepBackup = true
   prompt = false
-  cmd = nvim -f -c \"Gdiffsplit!\" \"\$MERGED\"
+  cmd = nvim -f -c "Gdiffsplit!" "\$MERGED"
 EOF
 chmod 0644 "$JB_ENV_DIR/.gitconfig"
 
@@ -55,8 +68,17 @@ jb_check_for_executable() {
   type \$1 >/dev/null 2>/dev/null
 }
 
+jb_sudo() {
+  if [ "\$1" = "su" ] && [ "\$2" = "-" ]; then
+    \sudo --preserve-env=TMUX su - --whitelist-environment=TMUX \\
+      -c 'JB_SKIP_TMUX_UPDATE=1; eval "\`$JB_FETCH_EXE $JB_FETCH_FLAGS https://env.joycebabu.com\`"'
+  else
+    \sudo "\$@"
+  fi
+}
+
 if jb_check_for_executable nvim; then
-  alias vim='nvim -c "let g:tty='\''$(tty)'\''"'
+  alias vim='nvim -c "let g:tty='\''\$(tty)'\''"'
 elif ! jb_check_for_executable vim; then
   alias vim='vi'
 fi
@@ -69,9 +91,8 @@ unset jb_check_for_executable
 [ -f "\$HOME/.\${SHELL}rc" ] && . "\$HOME/.\${SHELL}rc"
 
 alias tmux='\tmux -f"$JB_ENV_DIR/.tmux.conf"'
-alias sudo='\sudo --preserve-env=VIMINIT,TMUX,JB_ENV_DIR'
-#alias ssu='\sudo --preserve-env=VIMINIT,TMUX,JB_ENV_DIR su --whitelist-environment=VIMINIT,TMUX,JB_ENV_DIR'
-alias ssu="\sudo --preserve-env=TMUX su - --whitelist-environment=TMUX -c 'source <(curl -s https://env.joycebabu.com); $SHELL"
+alias sd='\sudo --preserve-env=VIMINIT,TMUX,JB_ENV_DIR'
+alias sudo='jb_sudo'
 EOF
 
 chmod 0644 "$JB_ENV_DIR/.shrc"
@@ -80,8 +101,8 @@ jb_dl_config_file vim/.config/nvim/basic.vim .vimrc
 
 # Setup shell
 for ENV_SHELL in zsh bash `echo $SHELL|rev|cut -d/ -f1|rev`; do
-  if jb_check_for_executable $ENV_SHELL; then
-    ENV_TMUX_DEF_CMD=`which $ENV_SHELL`
+  if jb_check_for_executable "$ENV_SHELL"; then
+    JB_ENV_TMUX_DEF_CMD=$(which "$ENV_SHELL")
     break
   fi
 done
@@ -90,7 +111,7 @@ if [ "zsh" = "$ENV_SHELL" ]; then
   export JB_ZDOTDIR=$JB_ENV_DIR
   ln -sf $JB_ENV_DIR/.shrc $JB_ENV_DIR/.zshrc
 elif [ "bash" = "$ENV_SHELL" ]; then
-  ENV_TMUX_DEF_CMD="$ENV_TMUX_DEF_CMD --rcfile $JB_ENV_DIR/.shrc"
+  JB_ENV_TMUX_DEF_CMD="$JB_ENV_TMUX_DEF_CMD --rcfile $JB_ENV_DIR/.shrc"
 else
   JB_ENV="$JB_ENV_DIR/.shrc"
 fi
@@ -98,12 +119,12 @@ fi
 find "$JB_ENV_DIR" -maxdepth 1 -type f -exec chmod 0644 {} +
 chmod 0755 "$JB_ENV_DIR"
 
-if jb_check_for_executable tmux; then
+if [ -z "$JB_SKIP_TMUX_UPDATE" ]; then
   # tmux installation detected
   echo "tmux found"
   jb_dl_config_file tmux/.tmux.conf .tmux.conf
 
-  echo "set-option -g default-command '$ENV_TMUX_DEF_CMD -i'" >> $JB_ENV_DIR/.tmux.conf
+  echo "set-option -g default-command '$JB_ENV_TMUX_DEF_CMD -i'" >> $JB_ENV_DIR/.tmux.conf
   if [ -n "$JB_ENV" ]; then
     echo "set-environment -g ENV '$JB_ENV'" >> $JB_ENV_DIR/.tmux.conf
   fi
@@ -114,22 +135,25 @@ if jb_check_for_executable tmux; then
   if [ -n "$TMUX" ]; then
     # Reload configuration if we are under a tmux session
     tmux source "$JB_ENV_DIR/.tmux.conf"
-  elif [ "`echo $TERM | cut -d- -f1`" != "screen" ]; then
+  elif [ "$(echo $TERM | cut -d- -f1)" != "screen" ]; then
     # Create a new tmux session, unless we are in a tmux session owned by
     # another user (after sudo su - user)
     tmux -f"$JB_ENV_DIR/.tmux.conf" new
   fi
 else
-  ENV="$JB_ENV" ZDOTDIR="$JB_ZDOTDIR" $ENV_TMUX_DEF_CMD -i
+  ENV="$JB_ENV" ZDOTDIR="$JB_ZDOTDIR" $JB_ENV_TMUX_DEF_CMD -i
 fi
-
 
 # Cleanup
 unset jb_dl_config_file
 unset jb_check_for_executable
-unset ENV_TMUX_DEF_CMD
+unset JB_ENV_TMUX_DEF_CMD
 unset JB_TMP_DIR
 unset JB_ENV_UID
 unset JB_ZDOTDIR
 unset JB_ENV
+unset JB_SKIP_TMUX_UPDATE
+unset JB_FETCH_EXE
+unset JB_FETCH_FLAGS
+unset JB_USER
 
