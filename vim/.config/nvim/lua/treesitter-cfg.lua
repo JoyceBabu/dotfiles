da69efd4f6717c3a4df3396f4e0208c4fae48d9d
Joyce Babu <joyce@ennexa.com>
2024-12-06 12:54:03 +0530

    feat(vim): autoinstall markdown and vim treesitter plugins

--- vim/.config/nvim/lua/treesitter-cfg.lua
+++ vim/.config/nvim/lua/treesitter-cfg.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "norg", "php", "javascript", "css", "typescript" },
+  ensure_installed = { "norg", "php", "javascript", "css", "typescript", 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
+  auto_install = true,
   highlight = {
     enable = true,
     custom_captures = {
       -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
       -- ["foo.bar"] = "Identifier",
     },
-    auto_install = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
